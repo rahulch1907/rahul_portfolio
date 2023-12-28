34b60c55d640d30583dee5483fdec2b643ce225a
Chinta Rahul Reddy <rahulch1907@gmail.com>
2023-12-28 13:28:33 +0530

    final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,9 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col items-center justify-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 h-40 sm:h-80 violet-gradient" />
         </div>
 
         <div>
@@ -21,14 +21,14 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Full Stack Developer || AWS || AI Enthusiast{" "}
-            <br className="sm:block hidden" />
+            
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -40,7 +40,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
             />
           </div>
         </a>
