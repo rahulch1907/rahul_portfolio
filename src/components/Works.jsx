ec6ef4722903f26b670e1fe1410eb9ff97fcd343
Chinta Rahul Reddy <rahulch1907@gmail.com>
2023-12-28 16:28:50 +0530

    final

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -31,18 +31,18 @@ const ProjectCard = ({
           <img
             src={image}
             alt="project_image"
-            className="w-full h-full object-cover rounded-3xl"
+            className="object-cover w-full h-full rounded-3xl"
           />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              className="flex items-center justify-center w-10 h-10 rounded-full cursor-pointer black-gradient"
             >
               <img
                 src={github}
                 alt="source code"
-                className="w-1/2 h-1/2 object-contain"
+                className="object-contain w-1/2 h-1/2"
               />
             </div>
           </div>
@@ -53,7 +53,7 @@ const ProjectCard = ({
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className="flex flex-wrap gap-2 mt-4">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -72,11 +72,11 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>My work</p>
+        <p className={`${styles.sectionSubText} text-center`}>My</p>
         <h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
       </motion.div>
 
-      <div className="w-full flex">
+      <div className="flex w-full">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-6xl leading-[30px]"
@@ -86,7 +86,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="flex flex-wrap mt-20 gap-7">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
